5bd284ec27fd3bdaed1631ca457e8ebdb3623221
Jérémy Mouton <jeremy.mouton@outscale.com>
2025-07-28 11:54:49 +0200

    🚸 chore: improve module output for have the corresponce between subnets and subregion

--- outputs.tf
+++ outputs.tf
@@ -32,3 +32,19 @@ output "public_ip_for_storage_nat_service" {
   description = "List of public IPs for NAT service in storage subnets."
   value       = [for ip in outscale_public_ip.nat_service_storage_subnet_public_ip : ip.public_ip]
 }
+
+output "private_subnet_ids_with_az" {
+  description = "List of private subnet IDs with their availability zones."
+  value = {
+    for subnet in outscale_subnet.private_subnet :
+    subnet.subnet_id => subnet.subregion_name
+  }
+}
+
+output "public_subnet_ids_with_az" {
+  description = "List of public subnet IDs with their availability zones."
+  value = {
+    for subnet in outscale_subnet.public_subnet :
+    subnet.subnet_id => subnet.subregion_name
+  }
+}
